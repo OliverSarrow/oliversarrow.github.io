d9af0316968d71328b3d48879f962061419ce068
OliverSarrow <sarrowoliver@gmail.com>
2025-03-02 21:26:53 -0500

    Delete 404.js

--- 404.js
+++
@@ -1,9 +0,0 @@
-export default function Custom404() {
-    return <div>
-            <h2>
-                Not exactly, please try again...
-            </h2>
-        </div>
-}
-
-// TODO: May also need a button returning to prev page.
