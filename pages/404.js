6d26993a18bd4a9ecf1bef5bac5ccb201ff8e373
OliverSarrow <sarrowoliver@gmail.com>
2025-03-02 20:14:20 -0500

    Create 404.js

---
+++ pages/404.js
@@ -0,0 +1,9 @@
+export default function Custom404() {
+    return <div>
+            <h2>
+                Not exactly, please try again...
+            </h2>
+        </div>
+}
+
+// TODO: May also need a button returning to prev page.
